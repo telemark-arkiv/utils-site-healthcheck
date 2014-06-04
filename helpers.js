957be41f523b91304f7e927779afd2ae8749d087
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-06-04 21:49:56 +0200

    Removes unused code

--- helpers.js
+++ helpers.js
@@ -2,8 +2,8 @@ var request = require('request')
   , cheerio = require('cheerio')
   , xml2js = require('xml2js')
   , parser = new xml2js.Parser()
-  , wcagValidator = require('wcag-validator')
-  , pagespeedAPIKey = 'insert-your-google-APIkey-here';
+  , pagespeedAPIKey = 'insert-your-google-APIkey-here'
+  ;
 
 function daysBetween(date1, date2){
   return Math.floor((date1 - date2)/(1000 * 60 * 60 * 24));
